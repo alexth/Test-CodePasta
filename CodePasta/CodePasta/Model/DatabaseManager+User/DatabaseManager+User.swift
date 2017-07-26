206280f78030395e407b730668289c6f86452bd7
Alex Golub <alexeythumb@gmail.com>
2017-07-26 10:53:18 +0300

    Add Databasemanager+User

--- CodePasta/CodePasta/Model/DatabaseManager+User/DatabaseManager+User.swift
+++ CodePasta/CodePasta/Model/DatabaseManager+User/DatabaseManager+User.swift
@@ -7,3 +7,83 @@
 //
 
 import Foundation
+import CoreData
+
+extension DatabaseManager {
+    func createUpdateUser(name: String,
+                          userID: String,
+                          creationDate: Date) -> (user: User?, error: NSError?) {
+        let fetchResponse = fetchUser()
+        if let error = fetchResponse.error {
+            return (nil, error)
+        } else {
+            let user: User!
+            if let fetchedUser = fetchResponse.user {
+                user = fetchedUser
+            } else {
+                user = NSEntityDescription.insertNewObject(forEntityName: "User", into: managedObjectContext) as! User
+                user.userID = userID
+            }
+            user.name = name
+            user.creationDate = creationDate as NSDate
+            do {
+                try managedObjectContext.save()
+                return (user, nil)
+            } catch {
+                return (nil, error as NSError)
+            }
+        }
+    }
+
+    func users() -> [User] {
+        var usersArray = [User]()
+        let fetchRequest = userFetchRequest()
+        do {
+            usersArray = try managedObjectContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            // TODO: Handle issue properly
+            print("ERROR! Fetch failed: \(error.localizedDescription)")
+        }
+        return usersArray
+    }
+
+    func fetchUser() -> (user: User?, error: NSError?) {
+        var usersArray = [User]()
+        let fetchRequest = userFetchRequest()
+        do {
+            usersArray = try managedObjectContext.fetch(fetchRequest)
+            if usersArray.count == 1 {
+                if let user = usersArray.first {
+                    return (user, nil)
+                }
+            } else if usersArray.count == 0 {
+                return (nil, nil)
+            } else {
+                return (nil, userDuplicationError())
+            }
+        } catch let error as NSError {
+            return (nil, error)
+        }
+        return (nil, nil)
+    }
+
+    fileprivate func userFetchRequest() -> NSFetchRequest<User> {
+        let fetchRequest: NSFetchRequest<User> = User.fetchRequest()
+        fetchRequest.entity = NSEntityDescription.entity(forEntityName: "User", in: managedObjectContext)
+        return fetchRequest
+    }
+}
+
+fileprivate extension DatabaseManager {
+    fileprivate func userDuplicationError() -> NSError {
+        return NSError(domain: "userDuplicationError",
+                       code: 0,
+                       userInfo: [NSLocalizedDescriptionKey: "duplicate user found"])
+    }
+
+    fileprivate func noUsersError() -> NSError {
+        return NSError(domain: "noUsersError",
+                       code: 0,
+                       userInfo: [NSLocalizedDescriptionKey: "no users found"])
+    }
+}
