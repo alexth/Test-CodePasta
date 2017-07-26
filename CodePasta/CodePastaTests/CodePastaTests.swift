408eada4963e4a078039505ae68a42525109dd0d
Alex Golub <alexeythumb@gmail.com>
2017-07-26 08:33:14 +0300

    Add Xcode project

---
+++ CodePasta/CodePastaTests/CodePastaTests.swift
@@ -0,0 +1,36 @@
+//
+//  CodePastaTests.swift
+//  CodePastaTests
+//
+//  Created by Alex Golub on 7/26/17.
+//  Copyright © 2017 Alex Golub. All rights reserved.
+//
+
+import XCTest
+@testable import CodePasta
+
+class CodePastaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
