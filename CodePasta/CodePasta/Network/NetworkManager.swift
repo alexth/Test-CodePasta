b0988d67d0ea003b11565be38923321319e9427c
Alex Golub <alexeythumb@gmail.com>
2017-07-26 11:01:20 +0300

    Add NetworkManager

---
+++ CodePasta/CodePasta/Network/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  CodePasta
+//
+//  Created by Alex Golub on 7/26/17.
+//  Copyright © 2017 Alex Golub. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    fileprivate init() {}
+}
+
